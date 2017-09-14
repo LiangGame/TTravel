297009dd4cfb850cde5005bd21d43de260f16529
LiangGamg <liang_game@sina.com>
2017-09-14 10:10:43 +0800

    王文亮9.14-->>合并冲突文件

--- src/app/travel-notes/travel-notes.component.ts
+++ src/app/travel-notes/travel-notes.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelNotesComponent implements OnInit {
 
-<<<<<<< HEAD
 
-=======
->>>>>>> de443915655fcac2c9f61c56cbe34dfce0575802
   constructor() { }
 
   ngOnInit() {
