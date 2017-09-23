dfe3572cd442c2e23e2963687ecddbdf308860b2
iRYYY <1851546440@qq.com>
2017-09-23 14:47:14 +0800

    wenjian

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,8 +3,8 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class UserService {
-  url:string='http://127.0.0.1:8889/users';
-  // url:string='http://10.40.4.21:8889/users';
+  // url:string='http://127.0.0.1:8889/users';
+  url:string='http://10.40.4.21:8889/users';
 
   constructor(
     private http:HttpClient,
