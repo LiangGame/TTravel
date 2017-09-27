60708e3bfb12cc668bcaa56c5faa15eb3a2b28c9
listen to the rain <455978722@qq.com>
2017-09-27 09:31:22 +0800

    other姜皓文9.27

--- src/app/notes-child/notes-child.component.ts
+++ src/app/notes-child/notes-child.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any; // 在angular中调用jQ前的万能语句
 @Component({
   selector: 'app-notes-child',
   templateUrl: './notes-child.component.html',
@@ -10,6 +10,26 @@ export class NotesChildComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /*返回顶部*/
+    $(window).scroll(function () {
+      let height = $(window).scrollTop();
+      console.log(height);
+      if (height > 475) {
+        $('#to_top').show();
+      } else if (height <= 475) {
+        $('#to_top').hide();
+      }
+    });
+    $('#to_top').click(function () {
+      let height = $(window).scrollTop();
+      let inter = setInterval(function () {
+        if (height > 0) {
+          height -= 20;
+        } else {
+          clearInterval(inter);
+        }
+        $(window).scrollTop(height);
+      }, 10);
+    });
   }
-
 }
